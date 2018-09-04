6c5baccf77a4c3f54327342d131ae89f2334b636
Kirill Smirnov <smirnowku@gmail.com>
2018-09-04 19:35:31 +0300

    Accept message as request parameter

--- src/main/java/org/smirnowku/notifier/controller/NotificationController.java
+++ src/main/java/org/smirnowku/notifier/controller/NotificationController.java
@@ -6,10 +6,7 @@ import org.smirnowku.notifier.model.Channel;
 import org.smirnowku.notifier.model.Notification;
 import org.smirnowku.notifier.service.ChannelService;
 import org.smirnowku.notifier.service.NotificationService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/channels/{token}")
@@ -19,8 +16,8 @@ public class NotificationController {
     private final NotificationService notificationService;
     private final ChannelService channelService;
 
-    @PostMapping("/notify/{message}")
-    public Notification create(@PathVariable String token, @PathVariable String message) {
+    @PostMapping("/notify")
+    public Notification create(@PathVariable String token, @RequestParam String message) {
         Channel channel = channelService.getByToken(token);
         return notificationService.create(new NotificationCreate(message, channel));
     }
